009eb84ad1362bd5cf95f6806b45d66af5baad26
Enderboy <158325796+SaadWasim175@users.noreply.github.com>
2024-12-31 19:02:30 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+    return {
+      define: {
+        __APP_ENV__: process.env.VITE_VERCEL_ENV,
+  },
+};
   plugins: [react()],
 })
