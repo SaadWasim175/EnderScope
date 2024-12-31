46067f51b948d58dc3a33a84ddafdd47f6f1b312
Enderboy <158325796+SaadWasim175@users.noreply.github.com>
2024-12-31 19:25:09 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-    return {
-      define: {
-        __APP_ENV__: process.env.VITE_VERCEL_ENV,
-  },
-};
   plugins: [react()],
-})
+  define: {
+    'process.env.VITE_RAPIDAPI_KEY': JSON.stringify(process.env.VITE_RAPIDAPI_KEY),
+  },
+});
